4693daffcd88b0234114501f0f01086d428f4edf
Asad_Aliev <Asdaliev@mail.ru>
2023-03-18 12:41:35 +0300

    Сделал двухстороннюю передачу и сохранил в Client.py Server.py

--- Client.py
+++ Client.py
@@ -1,75 +1,95 @@
-# pip install opencv-python
 import socket, cv2, pickle, struct, imutils
 import pyshine as ps
+import threading
 
-mode = 'get'
-audio_get, context = ps.audioCapture(mode=mode)
 
-mode = 'send'
-audio_send, context = ps.audioCapture(mode=mode)
+def audio_stream():
+    # Socket Create
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host_ip = '192.168.1.9'
+    port = 8080
 
-# create socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = '192.168.1.9'  # Твой IP с помощью ipconfig в cmd
-port = 8080
-socket_address = (host_ip, port)
-client_socket.connect(socket_address)
+    socket_address = (host_ip, port)
+    client_socket.connect(socket_address)
+    print("CLIENT CONNECTED TO", socket_address)
 
-if client_socket:
-    vid = cv2.VideoCapture(0)  # Recording video
+    mode = 'get'
+    audio_get, context = ps.audioCapture(mode=mode)
+    mode = 'send'
+    audio_send, context = ps.audioCapture(mode=mode)
 
-while True:
     data = b""
     payload_size = struct.calcsize("Q")
+    while True:
+        # Send audio
+        frame = audio_send.get()
+        a = pickle.dumps(frame)
+        message = struct.pack("Q", len(a)) + a
+        client_socket.sendall(message)
+        # Receive audio
+        while len(data) < payload_size:
+            packet = client_socket.recv(4 * 1024)  # 4K
+            if not packet:
+                break
+            data += packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q", packed_msg_size)[0]
 
-    # send audio
-    frame = audio_send.get()
-    a = pickle.dumps(frame)
-    message = struct.pack("Q", len(a)) + a
-    client_socket.sendall(message)
+        while len(data) < msg_size:
+            data += client_socket.recv(4 * 1024)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        frame = pickle.loads(frame_data)
+        audio_get.put(frame)
 
-    # get audio
-    while len(data) < payload_size:
-        packet = client_socket.recv(4 * 1024)  # 4K
-        if not packet:
-            break
-        data += packet
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("Q", packed_msg_size)[0]
 
-    while len(data) < msg_size:
-        data += client_socket.recv(4 * 1024)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    frame = pickle.loads(frame_data)
-    audio_get.put(frame)
+def video_stream():
+    # Socket Create
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host_ip = '192.168.1.9'
+    port = 8080
 
-    # send video
-    img, frame = vid.read()
-    frame = imutils.resize(frame, width=320)
-    a = pickle.dumps(frame)
-    message = struct.pack("Q", len(a)) + a
-    client_socket.sendall(message)
-    cv2.imshow("TRANSMITTING VIDEO", frame)
+    socket_address = (host_ip, port-1)
+    client_socket.connect(socket_address)
+    print("CLIENT CONNECTED TO", socket_address)
 
-    # get video
-    while len(data) < payload_size:
-        packet = client_socket.recv(4 * 1024)  # 4K
-        if not packet:
-            break
-        data += packet
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("Q", packed_msg_size)[0]
-    while len(data) < msg_size:
-        data += client_socket.recv(4 * 1024)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    rframe = pickle.loads(frame_data)
-    cv2.imshow("RECEIVING VIDEO", rframe)
+    if client_socket:
+        vid = cv2.VideoCapture(0)  # Запись видео
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        client_socket.close()
-        exit(1)
+        data = b""
+        payload_size = struct.calcsize("Q")
+        while vid.isOpened() and client_socket:
+            # Send video
+            img, frame = vid.read()
+            frame = imutils.resize(frame, width=320)
+            a = pickle.dumps(frame)
+            message = struct.pack("Q", len(a)) + a
+            client_socket.sendall(message)
+            cv2.imshow("TRANSMITTING VIDEO", frame)
+            # Receive video
+            while len(data) < payload_size:
+                packet = client_socket.recv(4 * 1024)  # 4K
+                if not packet:
+                    break
+                data += packet
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("Q", packed_msg_size)[0]
+            while len(data) < msg_size:
+                data += client_socket.recv(4 * 1024)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            frame = pickle.loads(frame_data)
+            cv2.imshow("RECEIVING VIDEO", frame)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                client_socket.close()
+                exit(1)
+
+
+t1 = threading.Thread(target=audio_stream, args=())
+t2 = threading.Thread(target=video_stream, args=())
+t1.start()
+t2.start()
--- Server.py
+++ Server.py
@@ -1,87 +1,105 @@
-# pip install opencv-python
 import socket, cv2, pickle, struct, imutils
 import pyshine as ps
-
-mode = 'get'
-audio_get, context = ps.audioCapture(mode=mode)
-
-mode = 'send'
-audio_send, context = ps.audioCapture(mode=mode)
-
-# Socket Create
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = '192.168.1.9'
-port = 8080
-socket_address = (host_ip, port)
-
-# Socket Bind
-server_socket.bind(socket_address)
-
-# Socket Listen
-server_socket.listen(5)
-print("LISTENING AT:", socket_address)
-
-# Socket Accept
-
-client_socket, addr = server_socket.accept()
-print('GOT CONNECTION FROM:', addr)
-
-if client_socket:
-    vid = cv2.VideoCapture(0)  # Recording video
+import threading
 
 
-while True:
+def audio_stream():
+    # Socket Create
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host_ip = '192.168.1.9'
+    port = 8080
+    backlog = 5
+    socket_address = (host_ip, port)
+    print('STARTING SERVER AT', socket_address, '...')
+    server_socket.bind(socket_address)
+    server_socket.listen(backlog)
+
+    client_socket, addr = server_socket.accept()
+    print('GOT (audio) CONNECTION FROM:', addr)
+
+    mode = 'get'
+    audio_get, context = ps.audioCapture(mode=mode)
+    mode = 'send'
+    audio_send, context = ps.audioCapture(mode=mode)
+
     data = b""
     payload_size = struct.calcsize("Q")
+    while True:
+        # Receive audio
+        while len(data) < payload_size:
+            packet = client_socket.recv(4 * 1024)  # 4K
+            if not packet:
+                break
+            data += packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q", packed_msg_size)[0]
 
-    # get audio
-    while len(data) < payload_size:
-        packet = client_socket.recv(4 * 1024)  # 4K
-        if not packet:
-            break
-        data += packet
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("Q", packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += client_socket.recv(4 * 1024)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        frame = pickle.loads(frame_data)
+        audio_get.put(frame)
 
-    while len(data) < msg_size:
-        data += client_socket.recv(4 * 1024)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    frame = pickle.loads(frame_data)
-    audio_get.put(frame)
+        # Send audio
+        frame = audio_send.get()
+        a = pickle.dumps(frame)
+        message = struct.pack("Q", len(a)) + a
+        client_socket.sendall(message)
 
-    # send audio
-    frame = audio_send.get()
-    a = pickle.dumps(frame)
-    message = struct.pack("Q", len(a)) + a
-    client_socket.sendall(message)
 
-    # get video
-    while len(data) < payload_size:
-        packet = client_socket.recv(4 * 1024)  # 4K
-        if not packet:
-            break
-        data += packet
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("Q", packed_msg_size)[0]
-    while len(data) < msg_size:
-        data += client_socket.recv(4 * 1024)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    rframe = pickle.loads(frame_data)
-    cv2.imshow("RECEIVING VIDEO", rframe)
+def video_stream():
+    # Socket Create
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host_ip = '192.168.1.9'
+    port = 8080
+    socket_address = (host_ip, port-1)
+    server_socket.bind(socket_address)
+    server_socket.listen(5)
+    print("LISTENING AT:", socket_address)
 
-    # send video
-    img, frame = vid.read()
-    frame = imutils.resize(frame, width=320)
-    a = pickle.dumps(frame)
-    message = struct.pack("Q", len(a)) + a
-    client_socket.sendall(message)
-    cv2.imshow("TRANSMITTING VIDEO", frame)
+    # Socket Accept
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        client_socket.close()
-        exit(1)
+    client_socket, addr = server_socket.accept()
+    print('GOT (video) CONNECTION FROM:', addr)
+
+    data = b""
+    payload_size = struct.calcsize("Q")
+    if client_socket:
+        vid = cv2.VideoCapture(0)  # Запись видео
+
+        while vid.isOpened() and client_socket:
+            # Receive video
+            while len(data) < payload_size:
+                packet = client_socket.recv(4 * 1024)  # 4K
+                if not packet:
+                    break
+                data += packet
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("Q", packed_msg_size)[0]
+            while len(data) < msg_size:
+                data += client_socket.recv(4 * 1024)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            frame = pickle.loads(frame_data)
+            cv2.imshow("RECEIVING VIDEO", frame)
+            # Send video
+            img, frame = vid.read()
+            frame = imutils.resize(frame, width=320)
+            a = pickle.dumps(frame)
+            message = struct.pack("Q", len(a)) + a
+            client_socket.sendall(message)
+            cv2.imshow("TRANSMITTING VIDEO", frame)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                client_socket.close()
+                exit(1)
+
+
+t1 = threading.Thread(target=audio_stream, args=())
+t2 = threading.Thread(target=video_stream, args=())
+t1.start()
+t2.start()
