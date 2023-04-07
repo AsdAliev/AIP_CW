4bf60c0821296b44eed386cd3b6fc9779c50f2b6
Asad_Aliev <Asdaliev@mail.ru>
2023-04-07 23:44:40 +0300

    Замена библиотеки pyshine на pyaudio с целью повышения качества звука

--- Client.py
+++ Client.py
@@ -1,59 +1,60 @@
-import socket, cv2, pickle, struct, imutils
-import pyshine as ps
+import socket
+import cv2
+import pickle
+import struct
+import imutils
 import threading
+import pyaudio
+
+# Initialize PyAudio
+CHUNK_SIZE = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+p = pyaudio.PyAudio()
+
+# Open microphone stream
+stream_in = p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+                   input=True, frames_per_buffer=CHUNK_SIZE)
+
+# Open speaker stream
+stream_out = p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+                    output=True, frames_per_buffer=CHUNK_SIZE)
+
+HOST_IP = '192.168.1.9'  # ip from ipconfig
+PORT = 8080  # Any port
+
+# Socket Create
+client_socket_audio = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_address_audio = (HOST_IP, PORT)
+client_socket_audio.connect(socket_address_audio)
+print("CLIENT CONNECTED TO", socket_address_audio)
 
 
-def audio_stream():
-    # Socket Create
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host_ip = '192.168.1.9'
-    port = 8080
-
-    socket_address = (host_ip, port)
-    client_socket.connect(socket_address)
-    print("CLIENT CONNECTED TO", socket_address)
-
-    mode = 'get'
-    audio_get, context = ps.audioCapture(mode=mode)
-    mode = 'send'
-    audio_send, context = ps.audioCapture(mode=mode)
-
-    data = b""
-    payload_size = struct.calcsize("Q")
+def send_audio():
     while True:
-        # Send audio
-        frame = audio_send.get()
-        a = pickle.dumps(frame)
-        message = struct.pack("Q", len(a)) + a
-        client_socket.sendall(message)
-        # Receive audio
-        while len(data) < payload_size:
-            packet = client_socket.recv(4 * 1024)  # 4K
-            if not packet:
-                break
-            data += packet
-        packed_msg_size = data[:payload_size]
-        data = data[payload_size:]
-        msg_size = struct.unpack("Q", packed_msg_size)[0]
+        # Read audio data from the microphone stream
+        data = stream_in.read(CHUNK_SIZE)
+        # Send audio data to the client
+        client_socket_audio.sendall(data)
 
-        while len(data) < msg_size:
-            data += client_socket.recv(4 * 1024)
-        frame_data = data[:msg_size]
-        data = data[msg_size:]
-        frame = pickle.loads(frame_data)
-        audio_get.put(frame)
+
+def receive_audio():
+    while True:
+        # Receive audio data from the server
+        data = client_socket_audio.recv(CHUNK_SIZE)
+        # Play audio data on the speaker stream
+        stream_out.write(data)
 
 
 def video_stream():
     # Socket Create
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host_ip = '192.168.1.9'
-    port = 8080
-
-    socket_address = (host_ip, port-1)
+    socket_address = (HOST_IP, PORT-1)
     client_socket.connect(socket_address)
     print("CLIENT CONNECTED TO", socket_address)
 
+    print("Enter 'q' to close program")
     if client_socket:
         vid = cv2.VideoCapture(0)  # Запись видео
 
@@ -86,10 +87,13 @@ def video_stream():
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):
                 client_socket.close()
+                print("You entered 'q'")
                 exit(1)
 
 
-t1 = threading.Thread(target=audio_stream, args=())
-t2 = threading.Thread(target=video_stream, args=())
+t1 = threading.Thread(target=receive_audio, args=())
+t2 = threading.Thread(target=send_audio, args=())
+t3 = threading.Thread(target=video_stream, args=())
 t1.start()
 t2.start()
+t3.start()
--- Server.py
+++ Server.py
@@ -1,73 +1,75 @@
-import socket, cv2, pickle, struct, imutils
-import pyshine as ps
+import socket
+import cv2
+import pickle
+import struct
+import imutils
 import threading
+import pyaudio
+
+# Initialize PyAudio
+CHUNK_SIZE = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+p = pyaudio.PyAudio()
+
+# Open microphone stream
+stream_in = p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+                   input=True, frames_per_buffer=CHUNK_SIZE)
+
+# Open speaker stream
+stream_out = p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+                    output=True, frames_per_buffer=CHUNK_SIZE)
+
+HOST_IP = '192.168.1.9'  # ip from ipconfig
+PORT = 8080  # Any port
+
+# Socket Create
+server_socket_audio = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+backlog = 5
+socket_address_audio = (HOST_IP, PORT)
+print('STARTING SERVER AT', socket_address_audio, '...')
+server_socket_audio.bind(socket_address_audio)
+server_socket_audio.listen(backlog)
+
+client_socket_audio, addr_audio = server_socket_audio.accept()
+print('GOT (audio) CONNECTION FROM:', addr_audio)
 
 
-def audio_stream():
-    # Socket Create
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host_ip = '192.168.1.9'
-    port = 8080
-    backlog = 5
-    socket_address = (host_ip, port)
-    print('STARTING SERVER AT', socket_address, '...')
-    server_socket.bind(socket_address)
-    server_socket.listen(backlog)
-
-    client_socket, addr = server_socket.accept()
-    print('GOT (audio) CONNECTION FROM:', addr)
-
-    mode = 'get'
-    audio_get, context = ps.audioCapture(mode=mode)
-    mode = 'send'
-    audio_send, context = ps.audioCapture(mode=mode)
-
-    data = b""
-    payload_size = struct.calcsize("Q")
+def send_audio():
     while True:
-        # Receive audio
-        while len(data) < payload_size:
-            packet = client_socket.recv(4 * 1024)  # 4K
-            if not packet:
-                break
-            data += packet
-        packed_msg_size = data[:payload_size]
-        data = data[payload_size:]
-        msg_size = struct.unpack("Q", packed_msg_size)[0]
+        # Read audio data from the microphone stream
+        data = stream_in.read(CHUNK_SIZE)
+        # Send audio data to the client
+        client_socket_audio.sendall(data)
 
-        while len(data) < msg_size:
-            data += client_socket.recv(4 * 1024)
-        frame_data = data[:msg_size]
-        data = data[msg_size:]
-        frame = pickle.loads(frame_data)
-        audio_get.put(frame)
 
-        # Send audio
-        frame = audio_send.get()
-        a = pickle.dumps(frame)
-        message = struct.pack("Q", len(a)) + a
-        client_socket.sendall(message)
+def receive_audio():
+    while True:
+        # Receive audio data from the server
+        data = client_socket_audio.recv(CHUNK_SIZE)
+        # Play audio data on the speaker stream
+        stream_out.write(data)
 
 
 def video_stream():
     # Socket Create
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host_ip = '192.168.1.9'
-    port = 8080
-    socket_address = (host_ip, port-1)
+    socket_address = (HOST_IP, PORT - 1)
     server_socket.bind(socket_address)
     server_socket.listen(5)
     print("LISTENING AT:", socket_address)
 
     # Socket Accept
-
     client_socket, addr = server_socket.accept()
     print('GOT (video) CONNECTION FROM:', addr)
+
+    print("Enter 'q' to close program")
 
     data = b""
     payload_size = struct.calcsize("Q")
     if client_socket:
-        vid = cv2.VideoCapture(0)  # Запись видео
+        vid = cv2.VideoCapture(0)  # Recording video
 
         while vid.isOpened() and client_socket:
             # Receive video
@@ -96,10 +98,13 @@ def video_stream():
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):
                 client_socket.close()
+                print("You entered 'q'")
                 exit(1)
 
 
-t1 = threading.Thread(target=audio_stream, args=())
-t2 = threading.Thread(target=video_stream, args=())
+t1 = threading.Thread(target=send_audio, args=())
+t2 = threading.Thread(target=receive_audio, args=())
+t3 = threading.Thread(target=video_stream, args=())
 t1.start()
 t2.start()
+t3.start()
